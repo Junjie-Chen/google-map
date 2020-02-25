a6a7b78d2dc9e295694a8b5534cc41a009dd49ca
Junjie Chen <junjie.chen18@gmail.com>
2020-02-24 20:59:13 -0500

    feat(map): initialize map field with instance of Map class

--- src/Map.ts
+++ src/Map.ts
@@ -1,3 +1,13 @@
 export class Map {
   private map: google.maps.Map;
+
+  constructor(mapId: string) {
+    this.map = new google.maps.Map(document.getElementById(mapId), {
+      center: {
+        lat: 0,
+        lng: 0
+      },
+      zoom: 2
+    });
+  }
 }
