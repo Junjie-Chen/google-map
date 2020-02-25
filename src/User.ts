4765e3cd91f3795669656861350945f38ec28989
Junjie Chen <junjie.chen18@gmail.com>
2020-02-24 21:17:44 -0500

    feat(user): initialize name field with randomly generated first name

--- src/User.ts
+++ src/User.ts
@@ -1,7 +1,13 @@
+import faker from 'faker';
+
 export class User {
   name: string;
   location: {
     latitude: number;
     longitude: number;
   };
+
+  constructor() {
+    this.name = faker.name.firstName();
+  }
 }
